61b0d76875547c25e84ceefe5cd2f2079d3cffbc
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2015-08-18 19:55:50 -0300

    Refatora exemplo ibge.py

--- brasil.io/rio/exemplos/ibge.py
+++ brasil.io/rio/exemplos/ibge.py
@@ -26,32 +26,34 @@ def indicador_por_uf(indicador, uf):
     resposta = requests.get(URL_DADOS, parametros)
     html = resposta.content.decode('utf-8')
 
-    with rows.locale_context(('pt_BR', 'UTF-8')):
+    with rows.locale_context('pt_BR.UTF-8'):
         tabela = rows.import_from_html(html)
 
     return tabela
 
+
+def indicador_brasil(indicador):
+    tabelas = []
+    for uf in CODIGO_UF:
+        print('Baixando {} para {}...'.format(indicador, uf))
+        tabela = indicador_por_uf(indicador, uf)
+        tabelas.append(tabela)
+    tabela_brasil = sum(tabelas)
+    return tabela_brasil
+
+
 if __name__ == '__main__':
-    #for info in indicador_por_uf('populacao', 'RJ'):
-    #    print('{} => {} habitantes'.format(info.municipio, info.pessoas))
 
-    populacao_brasil = []
-    for uf in CODIGO_UF:
-        print('Baixando população para {}...'.format(uf))
-        resultado = indicador_por_uf('populacao', uf)
-        populacao_brasil.append(resultado)
-    tabela_populacao_brasil = sum(populacao_brasil)
-    rows.export_to_csv(tabela_populacao_brasil, 'populacao-brasil.csv')
+    # Baixa população para todos UFs e salva em 'populacao-brasil.csv'
+    populacao_brasil = indicador_brasil('populacao')
+    rows.export_to_csv(populacao_brasil, 'populacao-brasil.csv')
 
-    area_brasil = []
-    for uf in CODIGO_UF:
-        print('Baixando área para {}...'.format(uf))
-        resultado = indicador_por_uf('area', uf)
-        area_brasil.append(resultado)
-    tabela_area_brasil = sum(area_brasil)
-    rows.export_to_csv(tabela_area_brasil, 'area-brasil.csv')
+    # Baixa área para todos UFs e salva em 'area-brasil.csv'
+    area_brasil = indicador_brasil('area')
+    rows.export_to_csv(area_brasil, 'area-brasil.csv')
 
+    # Junta tabelas de população e área e salva em 'brasil.csv'
     tabela_geral = rows.join(keys=('uf', 'municipio'),
-                             tables=[tabela_populacao_brasil,
-                                     tabela_area_brasil])
+                             tables=[populacao_brasil,
+                                     area_brasil])
     rows.export_to_csv(tabela_geral, 'brasil.csv')
