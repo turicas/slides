52a7489993e85ee548f6de248462987430028124
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2015-08-29 10:29:23 -0300

    Conserta script com versão nova de rows

--- brasil.io/uaipython/exemplos/municipios_rj.py
+++ brasil.io/uaipython/exemplos/municipios_rj.py
@@ -2,15 +2,17 @@
 
 from __future__ import unicode_literals
 
+from io import BytesIO
+
 import requests
 import rows
 
 
 url = 'http://cidades.ibge.gov.br/comparamun/compara.php?coduf=33&idtema=16&codv=v08'
 resposta = requests.get(url)
-html = resposta.content.decode('utf-8')
+html = resposta.content
 with rows.locale_context('pt_BR.UTF-8'):
-    tabela = rows.import_from_html(html)
+    tabela = rows.import_from_html(BytesIO(html))
 
 for municipio in tabela:
     print municipio
